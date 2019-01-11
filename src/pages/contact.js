1c30b0ca7c85ab163fb0129a091b980060b89cad
zacharyarney <zachary.arney@gmail.com>
2019-01-11 08:38:25 -0800

    personalizing the blog

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -52,7 +52,7 @@ const Contact = () => (
       </Header>
       <Content>
         <h1>Contact</h1>
-        <p>Super cool intro text to get people contacting me! It uses Netlify's form feature.</p>
+        <p>Let me know what's up.</p>
         <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
           <p>
             <label htmlFor="contact-name">
--- src/pages/index.js
+++ src/pages/index.js
@@ -47,8 +47,7 @@ const IndexPage = ({
       <Hero>
         <h1>Hi.</h1>
         <p>
-          I&apos;m John Doe, a Senior UX Developer with five years of industry experience, specializing in developing
-          React apps with the best UX users can get.
+          I&apos;m Zachary Arney, a web dev student at Lambda School. This is my blog!
         </p>
         <Link to="/contact">
           <Button big>
